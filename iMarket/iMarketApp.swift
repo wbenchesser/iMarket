ddbeda69ee0a108dfdda991a8d526fbe928e49b2
William Chesser <bchesser@unc.edu>
2024-08-23 14:58:44 -0400

    Initial Commit

---
+++ iMarket/iMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMarketApp.swift
+//  iMarket
+//
+//  Created by Ben Chesser on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct iMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
